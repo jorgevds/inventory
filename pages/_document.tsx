ced0eda1d0a400c6791d6d9fcc91b2e03d968cbe
jorgevds <jorgevandesompel@gmail.com>
2020-10-19 12:59:26 +0200

    styled _document page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ class MyDocument extends Document {
     return (
       <Html lang="eng">
         <Head />
-        <body>
+        <body className="font-body">
           <Main />
           <NextScript />
         </body>
