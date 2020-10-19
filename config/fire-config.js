30ec30775b88e02ecb9e1ffad02a1858c6065892
jorgevds <jorgevandesompel@gmail.com>
2020-10-19 12:53:59 +0200

    firebase config keys now in .env.local file

--- config/fire-config.js
+++ config/fire-config.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBYAr61oMF8r5KEQka3sPPPDIaDBop2g70",
-  authDomain: "inventory-959f4.firebaseapp.com",
-  databaseURL: "https://inventory-959f4.firebaseio.com",
-  projectId: "inventory-959f4",
-  storageBucket: "inventory-959f4.appspot.com",
-  messagingSenderId: "688725914808",
-  appId: "1:688725914808:web:f00c6bc946e6203f04b04d",
-  measurementId: "G-8K4E0PJK2Z",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 try {
