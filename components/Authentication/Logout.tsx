0cf327aec2d496ceb6f3040412c6fc84f8a8d980
jorgevds <jorgevandesompel@gmail.com>
2020-10-19 12:57:33 +0200

    styled logout component

--- components/Authentication/Logout.tsx
+++ components/Authentication/Logout.tsx
@@ -16,7 +16,13 @@ const Logout = () => {
       });
   };
 
-  return <button onClick={handleLogout} className="flex justify-end flex-auto py-4">Logout</button>;
+  return (
+  <div className="flex justify-end flex-auto">
+    <button onClick={handleLogout} className="px-4 my-4 text-white transition-all duration-300 ease-in-out transform border-2 border-solid rounded-lg active:bg-burgundyDark active:translate-y-1 focus:outline-none focus:shadow-outline hover:transition-all border-purple bg-blue hover:bg-burgundy">
+    Logout
+    </button>
+  </div>
+  );
 };
 
 export default Logout;
