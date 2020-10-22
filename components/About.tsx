61fcbb2ec1cbf1aff388dc46d1d921b0155c308e
jorgevds <jorgevandesompel@gmail.com>
2020-10-22 12:35:40 +0200

    about page coming soonTM

--- components/About.tsx
+++ components/About.tsx
@@ -1,30 +1,7 @@
 const About = () => {
   return (
-    <section className="flex-1">
-      <h1>Over ons</h1>
-      <div>
-        <h3>Contractor™ is een online bron van stielmensen uit elk beroep</h3>
-        <p>
-          Het is een webplatform voor bedrijven en freelancers om hun diensten
-          aan te bieden, en voor u om de juiste match voor de job te vinden.
-        </p>
-        <h3>Onze missie</h3>
-        <p>
-          Werken starten hoeft niet zo een moeilijk en formeel proces te zijn.
-          Daarom sloegen wij, het bij Contractor™, onze handen in elkaar en
-          gingen wij aan het werk.
-        </p>
-        <p>
-          Wij hopen traag maar zeker de kloof tussen business en klant te
-          dichten, met kwaliteit en een vlotte transactie in het oog.
-        </p>
-        <h3>Weg met de formaliteit</h3>
-        <p>
-          Nooit meer vol spanning een vraag voor een offerte insturen. Gedaan
-          met lange, saaie meetings waar je je toch nooit zeker voelt. Een
-          vlotte, snelle en kwaliteitsvolle samenwerking is de leidraad!
-        </p>
-      </div>
+    <section className="flex flex-col flex-1 w-3/5 min-h-screen pb-8 m-auto">
+      <p className="m-auto">Coming soon!</p>
     </section>
   );
 };
