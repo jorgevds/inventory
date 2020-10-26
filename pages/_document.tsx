0652cfb1a71f52fff2fcae72a764daa8d8112f0a
jorgevds <jorgevandesompel@gmail.com>
2020-10-26 11:12:15 +0100

    fixed typo in html lang attribute

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="eng">
+      <Html lang="en">
         <Head />
         <body className="font-body">
           <Main />
