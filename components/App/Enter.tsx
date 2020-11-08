b6abaeabfbe4d1fb225baacb8851a8d632d144c2
jorgevds <jorgevandesompel@gmail.com>
2020-11-08 23:20:49 +0100

    now reroutes when not logged in and the user changes the URL manually to /enter

--- components/App/Enter.tsx
+++ components/App/Enter.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import fire from "../../config/fire-config";
 import { useRouter } from "next/router";
 
@@ -49,6 +49,12 @@ const EnterForm: React.FC<Props> = ({ loggedIn }) => {
     setItemAmount(1);
     setSubmit(true);
   };
+
+  useEffect(() => {
+    if (!loggedIn) {
+      router.push("/users/login");
+    }
+  }, []);
 
   return (
     <section className="flex flex-col flex-1 pb-8 m-auto minmd:w-3/5">
