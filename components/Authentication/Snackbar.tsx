58d4e6674e7476cc019da6d509d00c050cdaf2ca
jorgevds <jorgevandesompel@gmail.com>
2022-08-02 21:06:26 +0200

    Fixed the snackbar to the bottom of the application at all times. Agreeing now sets a cookie to remember having clicked on, and no longer disappears on log in regardless of oking.

--- components/Authentication/Snackbar.tsx
+++ components/Authentication/Snackbar.tsx
@@ -1,36 +1,63 @@
 import Link from "next/link";
-import { useState } from "react";
-import { useAuth } from "../Authentication/AuthContext";
+import { useEffect, useState } from "react";
 
 const Snackbar = () => {
-  const [agree, setAgree] = useState<boolean>(false);
-  const { loggedIn } = useAuth();
+    const [agree, setAgree] = useState<boolean>(false);
 
-  return (
-    <>
-      {!loggedIn && !agree && (
-        <section className="sticky bottom-0 z-20 w-full py-2 bg-white minmd:px-12">
-          <article className="flex flex-col m-auto minlg:w-3/6 minmd:px-12 sm:w-full sm:px-4">
-            <h2>Inventory uses cookies</h2>
-            <article className="flex">
-              <p className="sm:w-3/5">
-                But only to allow for longer login sessions. See our{" "}
-                <Link href="/privacy">
-                  <a className="text-blue">privacy notice</a>
-                </Link>{" "}
-                for more information.
-              </p>
-              <button
-                onClick={() => setAgree(true)}
-                className="px-4 py-2 m-auto text-white transition-all duration-300 ease-in-out transform rounded-lg shadow-md active:bg-blueDark focus:outline-none focus:shadow-outline hover:transition-all bg-blue active:translate-y-1 hover:scale-105"
-              >
-                Ok
-              </button>
-            </article>
-          </article>
-        </section>
-      )}
-    </>
-  );
+    const setCookie = () => {
+        const date = new Date();
+        date.setTime(date.getTime() + 365 * 24 * 60 * 60 * 1000);
+        const expires = "; expires=" + date.toUTCString();
+
+        document.cookie =
+            "Inventory privacy policy cookie=" + expires + "; path=/";
+        setAgree(true);
+    };
+
+    useEffect(() => {
+        const cookies: string[] = document.cookie.split(";");
+        const privacyCookie = cookies.find((cookie) =>
+            cookie.includes("Inventory privacy policy cookie"),
+        );
+
+        if (privacyCookie) {
+            setAgree(true);
+        }
+    }, []);
+
+    return (
+        <>
+            {!agree ? (
+                <article className="fixed bottom-0 z-20 w-full py-2 bg-white minmd:px-12">
+                    <section className="flex minmd:px-12 sm:px-4 minlg:w-4/6 m-auto  sm:w-full">
+                        <section>
+                            <h2>Inventory uses cookies</h2>
+                            <section className="minlg:w-4/5">
+                                <p className="sm:w-4/5">
+                                    But only to allow for longer login sessions.
+                                    See our{" "}
+                                    <Link href="/privacy">
+                                        <a className="text-blue">
+                                            privacy notice
+                                        </a>
+                                    </Link>{" "}
+                                    for more information.
+                                </p>
+                            </section>
+                        </section>
+                        <section className="m-auto">
+                            <button
+                                onClick={() => setCookie()}
+                                className="px-4 py-2 m-auto text-white transition-all duration-300 ease-in-out transform rounded-lg shadow-md active:bg-blueDark focus:outline-none focus:shadow-outline hover:transition-all bg-blue active:translate-y-1 hover:scale-105"
+                            >
+                                Ok
+                            </button>
+                        </section>
+                    </section>
+                </article>
+            ) : null}
+        </>
+    );
 };
+
 export default Snackbar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,27 +3,29 @@ import { ToastContainer, Slide } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
 import { AppProps } from "next/app";
 import { AuthProvider } from "../components/Authentication/AuthContext";
+import Snackbar from "../components/Authentication/Snackbar";
 
 function MyApp(props: AppProps) {
-  const { Component, pageProps } = props;
+    const { Component, pageProps } = props;
 
-  return (
-    <AuthProvider>
-      <Component {...pageProps} />
-      <ToastContainer
-        position="top-center"
-        transition={Slide}
-        autoClose={3000}
-        hideProgressBar
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss={false}
-        draggable={false}
-        pauseOnHover
-      />
-    </AuthProvider>
-  );
+    return (
+        <AuthProvider>
+            <Component {...pageProps} />
+            <ToastContainer
+                position="top-center"
+                transition={Slide}
+                autoClose={3000}
+                hideProgressBar
+                newestOnTop
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss={false}
+                draggable={false}
+                pauseOnHover
+            />
+            <Snackbar />
+        </AuthProvider>
+    );
 }
 
 export default MyApp;
