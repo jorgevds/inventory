b60867ebd4272cbfa463a0a0ccf6f26812d3bd93
jorgevds <jorgevandesompel@gmail.com>
2022-08-14 23:00:35 +0200

    Updated all packages. Added specific import paths to tsconfig.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 module.exports = {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
     future: {
         removeDeprecatedGapUtilities: true,
         purgeLayersByDefault: true,
     },
-    purge: [],
     theme: {
         extend: {},
         borderRadius: {
